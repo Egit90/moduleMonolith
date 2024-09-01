2b606b28802b995b19056865baa1ae493c171558
egit90 <elie90@gmail.com>
2024-09-01 13:57:15 -0400

    added Efcore to the DI container for the catalog module

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Catalog.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -8,6 +10,8 @@ public static class CatalogModule
 {
     public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration Configuration)
     {
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+        services.AddDbContext<CatalogDb>(opt => opt.UseNpgsql(connectionString));
         return services;
     }
     public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
