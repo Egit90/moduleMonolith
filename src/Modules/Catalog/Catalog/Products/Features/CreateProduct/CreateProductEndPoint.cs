58e79d900275c77c37ed96d5bde92e413a2ea25d
egit90 <elie90@gmail.com>
2024-09-02 21:41:49 -0400

    CreateProductEndPoint

---
+++ src/Modules/Catalog/Catalog/Products/Features/CreateProduct/CreateProductEndPoint.cs
@@ -0,0 +1,33 @@
+using Carter;
+using Catalog.Products.Dtos;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Catalog.Products.Features.CreateProduct;
+
+public sealed record CreateProductRequest(ProductDto Product);
+public sealed record CreateProductResponse(Guid Id);
+
+
+public sealed class CreateProductEndPoint : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/product", async (CreateProductRequest request, ISender sender) =>
+        {
+            var command = request.Adapt<CreateProductCommand>();
+            var commandRes = await sender.Send(command);
+            var res = commandRes.Adapt<CreateProductResponse>();
+
+            return Results.Created($"/products/{res.Id}", res);
+        })
+        .WithName("CreateProduct")
+        .Produces<CreateProductResponse>(StatusCodes.Status201Created)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Create Product")
+        .WithDescription("Create Product");
+    }
+}
