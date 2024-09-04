33cc4d662fd7e49742f39390962af91a1d9365b7
egit90 <elie90@gmail.com>
2024-09-04 10:59:33 -0400

    Add and Remove Items from Basket

---
+++ src/Modules/Basket/Basket/Basket/Features/AddItemIntoBasket/AddItemIntoBasketHandler.cs
@@ -0,0 +1,46 @@
+using System.Windows.Input;
+using Basket.Basket.Dtos;
+using Basket.Basket.Exceptions;
+using Basket.Basket.Models;
+using Basket.Data;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Shared.CQRS;
+
+namespace Basket.Basket.Features.AddItemIntoBasket;
+
+public sealed record AddItemIntoBasketCommand(string UserName, ShoppingCartItemDto ShoppingCartItem) : ICommand<AddItemIntoBasketResult>;
+public sealed record AddItemIntoBasketResult(Guid Id);
+
+public sealed class AddItemIntoBasketCommandValidator : AbstractValidator<AddItemIntoBasketCommand>
+{
+    public AddItemIntoBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        RuleFor(x => x.ShoppingCartItem.ProductId).NotEmpty().WithMessage("ProductId is required");
+        RuleFor(x => x.ShoppingCartItem.Quantity).GreaterThan(0).WithMessage("Quantity must be greater than 0");
+    }
+}
+
+internal sealed class AddItemIntoBasketHandler(BasketDbContext context) : ICommandHandler<AddItemIntoBasketCommand, AddItemIntoBasketResult>
+{
+    public async Task<AddItemIntoBasketResult> Handle(AddItemIntoBasketCommand command, CancellationToken cancellationToken)
+    {
+        var shoppingCart = await context.ShoppingCarts
+                            .Include(x => x.Items)
+                            .SingleOrDefaultAsync(x => x.UserName == command.UserName, cancellationToken: cancellationToken)
+                            ?? throw new BasketNotFoundException(command.UserName);
+
+        shoppingCart.AddItem(
+            command.ShoppingCartItem.ProductId,
+            command.ShoppingCartItem.Quantity,
+            command.ShoppingCartItem.Color,
+            command.ShoppingCartItem.price,
+            command.ShoppingCartItem.ProductName);
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        return new AddItemIntoBasketResult(shoppingCart.Id);
+
+    }
+}
---
+++ src/Modules/Basket/Basket/Basket/Features/RemoveItemFromBasket/RemoveItemFromBasketHandler.cs
@@ -0,0 +1,38 @@
+
+using Basket.Basket.Exceptions;
+using Basket.Data;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Shared.CQRS;
+
+namespace Basket.Basket.Features.RemoveItemFromBasket;
+
+public sealed record RemoveItemFromBasketCommand(string UserName, Guid ProductId) : ICommand<RemoveItemFromBasketResult>;
+public sealed record RemoveItemFromBasketResult(Guid Id);
+
+public sealed class RemoveItemFromBasketCommandValidator : AbstractValidator<RemoveItemFromBasketCommand>
+{
+    public RemoveItemFromBasketCommandValidator()
+    {
+        RuleFor(x => x.UserName).NotEmpty().WithMessage("UserName is required");
+        RuleFor(x => x.ProductId).NotEmpty().WithMessage("ProductId is required");
+    }
+}
+
+internal sealed class RemoveItemFromBasketHandler(BasketDbContext context) : ICommandHandler<RemoveItemFromBasketCommand, RemoveItemFromBasketResult>
+{
+    public async Task<RemoveItemFromBasketResult> Handle(RemoveItemFromBasketCommand command, CancellationToken cancellationToken)
+    {
+        var cart = await context.ShoppingCarts
+                                .SingleOrDefaultAsync(x => x.UserName == command.UserName, cancellationToken: cancellationToken)
+                                ?? throw new BasketNotFoundException(command.UserName);
+
+        cart.RemoveItem(command.ProductId);
+
+        await context.SaveChangesAsync(cancellationToken);
+
+        return new RemoveItemFromBasketResult(cart.Id);
+
+
+    }
+}
