a1db95a539704327ce122271ace245b087a8ad13
egit90 <elie90@gmail.com>
2024-09-04 09:41:47 -0400

    basket migration

--- src/Modules/Basket/Basket/BasketModule.cs
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -1,6 +1,10 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Basket.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Shared.Data.Interceptors;
 
 namespace Basket;
 
@@ -8,6 +12,17 @@ public static class BasketModule
 {
     public static IServiceCollection AddBasketModule(this IServiceCollection services, IConfiguration Configuration)
     {
+        var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventInterceptor>();
+
+        services.AddDbContext<BasketDbContext>((sp, opt) =>
+        {
+            opt.AddInterceptors(sp.GetService<ISaveChangesInterceptor>()!);
+            opt.UseNpgsql(connectionString);
+        });
+
         return services;
     }
 
