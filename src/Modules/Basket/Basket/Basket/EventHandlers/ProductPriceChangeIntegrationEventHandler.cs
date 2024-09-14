91d5ffab84186e499acafcf2f8ffe9644ce8f223
egit90 <elie90@gmail.com>
2024-09-14 16:46:10 -0400

    dsa

--- src/Modules/Basket/Basket/Basket/EventHandlers/ProductPriceChangeIntegrationEventHandler.cs
+++ src/Modules/Basket/Basket/Basket/EventHandlers/ProductPriceChangeIntegrationEventHandler.cs
@@ -1,3 +1,4 @@
+using Basket.Basket.Features.UpdateItemPriceInBasket;
 using MassTransit;
 using MediatR;
 using Microsoft.Extensions.Logging;
@@ -8,12 +9,20 @@ namespace Basket.Basket.EventHandlers;
 public sealed class ProductPriceChangeIntegrationEventHandler
 (ILogger<ProductPriceChangeIntegrationEventHandler> logger, ISender sender) : IConsumer<ProductPriceChangeIntegrationEvent>
 {
-    public Task Consume(ConsumeContext<ProductPriceChangeIntegrationEvent> context)
+    public async Task Consume(ConsumeContext<ProductPriceChangeIntegrationEvent> context)
     {
         logger.LogInformation("Integration Event Handled: {IntegrationEvent}", context.Message.GetType().Name);
         // Find basket Items with product id and update item price
 
         // mediator
-        return Task.CompletedTask;
+        var command = new UpdateItemPriceInBasketCommand(context.Message.ProductId, context.Message.Price);
+        var res = await sender.Send(command);
+
+        if (!res.IsSuccess)
+        {
+            logger.LogError("Error Updating a price in basket for product id: {productOT}", context.Message.ProductId);
+            return;
+        }
+        logger.LogError("Price for Product id : {productOT} was updated in Basket", context.Message.ProductId);
     }
 }
